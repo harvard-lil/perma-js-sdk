918c926edbbf95d062cdef0dd93c67181987f7aa
Matteo Cargnelutti <matteo.cargnelutti@gmail.com>
2022-06-15 12:59:22 -0400

    Init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+> 🚧 Work in progress
+
 # perma-js-sdk
 A JavaScript library to interact with Perma.cc's REST API
---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * perma-js-sdk
+ * @module index
+ * @author The Harvard Library Innovation Lab
+ * @license MIT
+ * @description Library for interacting with Perma.cc's REST API.
+ */
+export default class PermaApi {
+}
