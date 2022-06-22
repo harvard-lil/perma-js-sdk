baa45ecc49064121aa12273702b9a0b7853b3f39
Matteo Cargnelutti <matteo.cargnelutti@gmail.com>
2022-06-22 15:08:43 -0400

    Test environment setup

---
+++ index.test.js
@@ -0,0 +1,21 @@
+/**
+ * perma-js-sdk
+ * @module index.test
+ * @author The Harvard Library Innovation Lab
+ * @license MIT
+ * @description Unit and integration tests suite for `index`.
+ */
+/// <reference path="types.js" />
+
+import { PermaAPI } from "./index.js";
+
+//------------------------------------------------------------------------------
+// Suite description
+//------------------------------------------------------------------------------
+describe("Suite", () => {
+
+  test("Test", () => {
+    expect(true).toBe(true);
+  });
+
+});
